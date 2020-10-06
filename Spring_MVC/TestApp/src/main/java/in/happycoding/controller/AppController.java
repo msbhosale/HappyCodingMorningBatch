26e76c5483d2a30fb89a16072f6b389ddaff0b61
Madhusudan Bhosale <msbhosale@Madhusudans-MacBook-Air.local>
2020-10-06 09:05:51 +0530

    Spring MVC Demo

---
+++ Spring_MVC/TestApp/src/main/java/in/happycoding/controller/AppController.java
@@ -0,0 +1,16 @@
+package in.happycoding.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+	@RequestMapping("/")
+	public String indexPage() {
+		
+		return "profile";
+	}
+	
+	
+}
